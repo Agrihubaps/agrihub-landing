062fc59cebb93c552a743f4631e43687930ff2cf
Agrihubaps <agrihubaps@agrihubfarm.dk>
2025-10-17 20:32:04 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AgriHubLanding from "./AgriHubLanding";
+import AgriHubLanding from "./AgriHubLanding.tsx";  // указываем .tsx ЯВНО
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
