6c09ee6e4de422fb3030bd9a2424f789129f39e2
Agrihubaps <agrihubaps@agrihubfarm.dk>
2025-10-17 16:19:09 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";   
 import "./index.css";
+
+function Temp() {
+  return <div style={{padding:24}}>It works (Temp)</div>;
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Temp />
   </React.StrictMode>
 );
